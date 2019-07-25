80570461f336979e21b459ee3651bdded35d26a3
Manuel Blanc <ManuelBlanc@users.noreply.github.com>
2019-07-25 20:32:13 +0200

    Update and rename rat_apocalypse.md to rat_apocalypse.lua

--- _snippets/rat_apocalypse.lua
+++ _snippets/rat_apocalypse.lua
@@ -1,8 +1,8 @@
 ---
-title: critter_rat apocalypse [WIP]
+title: A critter_rat Tale: Innocence
+layout: snippet
 ---
 
-```lua
 local BREED_NAME = "critter_rat"
 local BREED_COUNT = 5
 
@@ -20,20 +20,20 @@ mod:hook_safe(ConflictDirector, "register_unit_killed", function(self, killed_un
     local position = POSITION_LOOKUP[killed_unit]
     local rotation = Unit.local_rotation(killed_unit, 0)
     local forward = Quaternion.forward(rotation)
-    local right = Quaternion.forward(rotation)
+    local right = Quaternion.right(rotation)
 
     AiUtils.generic_mutator_explosion(killed_unit, blackboard, "generic_mutator_explosion")
 
     for i=0, BREED_COUNT-1 do
         local ang = i/BREED_COUNT*pi2
-        local pos = position + forward*cos(ang) + left*sin(ang)
+        local pos = position + forward*cos(ang) + right*sin(ang)
         local ppos = LocomotionUtils.pos_on_mesh(nav_world, pos, 1, 1)
         if not ppos then
             ppos = GwNavQueries.inside_position_from_outside_position(nav_world, pos, 6, 6, 8, 0.5)
         end
         if ppos then
+            local rot = Quaternion.multiply(Quaternion.axis_angle(Vector3.up(), ang), rotation)
             spawn_unit(ppos, rot)
         end
     end
 end)
-```
